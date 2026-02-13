3b10b03475f646263a10a37abc79e71f7f9c13fb
Haythem Ftirich <haythemftirich194@gmail.com>
2026-02-13 13:57:32 +0100

    Week 2: First React component with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const courseTitle = "Advanced web dev CS220";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const studentName = "haythem ftirich";
+
+  const student = {
+    name: "haythem ftirich",
+    age: 21,
+    track: "CS220"
+  };
+
+  function sayHello() {
+  return `Hello ${studentName}`;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <h1>My React Project</h1>
+      <p>Student Name: {studentName}</p>
+      <p>Course: {courseTitle}</p>
+      <p>
+        Welcome to {courseTitle}, {studentName}!
       </p>
-    </>
-  )
+      <label  htmlFor="studentName">Enter your name:</label>
+      <input type="text" id="studentNameInput"/>
+      <h2>Informations</h2>
+      <p>Name: {student.name}</p>
+      <p>Age: {student.age}</p>
+      <p>Track: {student.track}</p>
+      <p>{sayHello()}</p>
+      
+       {/*
+        To answer the questions in the 8th step
+        1- i did understand well how to define variables either inside or outside a componant
+        2- im still confused when it comes to the defining a function inside another function and how to display it afterwards (i did use ai tools to comprehend that part)
+        3- i did make a mistake at first at displaying the object properties then i figured out that i should it this way:
+        <p>Name: {student.name}</p>
+        <p>Age: {student.age}</p>
+        <p>Track: {student.track}</p>
+        and not <p>{student}</p> because it won't work 
+      */}
+    </div>
+  );
 }
 
-export default App
+export default App;
